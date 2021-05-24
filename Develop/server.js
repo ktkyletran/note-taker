116620649a5c438db1b05a6779db9721096a60a9
Vanity <ktkyletran@gmail.com>
2021-05-24 13:31:22 -0500

    cleaned up file and moved api code to routes folder

--- Develop/server.js
+++ Develop/server.js
@@ -1,49 +1,24 @@
 // Dependencies
-const { json } = require('express');
 const express = require('express');
 const path = require('path');
 const fs = require ('fs');
 
 // Express configuration
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Loading Data
-const noteData = require('../Develop/db/db.json')
+const noteData = require('../Develop/db/noteData');
+const { Console } = require('console');
 
 // Data parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
-// HTML GET Requests
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-})
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-})
-
-// API Get Requests
-app.get('/api/notes', (req, res) => {res.json(noteData)});
-// app.get('/api/notes', (req, res) => {
-//   fs.readFile((path.join(__dirname, './db/db.json')), (err, data) => {
-//     if (err) {
-//       console.error(err)
-//       return;
-//     }
-//     else {
-//       res.send(data)
-//     }
-//   });
-// });
-
-// API Post Requests
-app.post('/api/notes', (req, res) => {
-  // res.sendFile(path.join(__dirname, './public/notes.html'));
-  res.redirect('/');
-  noteData.push(req.body);
-});
-
+// gives server routes to respond through
+require('./public/routes/apiRoutes')(app);
+require('./public/routes/htmlRoutes')(app);
 
 // Listener
 app.listen(PORT, () => {
